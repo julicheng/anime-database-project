653359b0c46d60bd03a1a4ecd8eab9f8c62c2719
Juli Cheng <94julicheng@gmail.com>
2018-03-12 17:18:34 +0000

    add array of genres for pretend database

--- public/staff/genres/index.php
+++ public/staff/genres/index.php
@@ -1,5 +1,16 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
+<?php
+
+$genres = [
+    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'Comedy'],
+    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Slice of Life'],
+    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'School'],
+    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Romance'],
+];
+
+?>
+
 <?php $page_title = "Genres"; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
