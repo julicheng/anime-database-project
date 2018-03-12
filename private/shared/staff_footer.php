1b899b67d224846271820ff4d7ed4d5d77ce4ddd
Juli Cheng <94julicheng@gmail.com>
2018-03-12 13:31:29 +0000

    move header and footer into separate files

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        &copy; <?php echo date('Y'); ?> Anime Database
+    </footer>
+</body>
+</html>
---
+++ private/shared/staff_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Anime Database Staff Area</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+</head>
+<body>
+    <header>
+        <h1>Anime Database Staff Area</h1>
+    </header>
+
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
