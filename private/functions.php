dd2a4f0b7cc29baab8958f663dd53441ae0edd20
Juli Cheng <94julicheng@gmail.com>
2018-03-12 22:57:26 +0000

    add check post or get request functions

--- private/functions.php
+++ private/functions.php
@@ -36,4 +36,12 @@ function redirect_to($location) {
     exit();
 }
 
+function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function is_get_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
 ?>
