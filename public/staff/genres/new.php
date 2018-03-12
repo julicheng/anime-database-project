1714fef529cc46cc01ced1f387f778e058c98fe8
Juli Cheng <94julicheng@gmail.com>
2018-03-12 19:11:05 +0000

    error handling test

---
+++ public/staff/genres/new.php
@@ -0,0 +1,18 @@
+<?php 
+
+require_once('../../../private/initialize.php');
+
+$test = isset($_GET['test']) ? $_GET['test'] : "";
+
+if ($test == '404') {
+    error_404();
+} elseif ($test == '500') {
+    error_500();
+} elseif ($test = "redirect") {
+    redirect_to(url_for('/staff/genres/index.php'));
+    exit();
+} else {
+    echo "No error";
+}
+
+?>
