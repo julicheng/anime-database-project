dcd8126c2322caeabdb127b0e09e1db9d9a901c7
Juli Cheng <94julicheng@gmail.com>
2018-03-12 17:09:45 +0000

    add content and shared_path file path

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,7 +1,18 @@
-<?php require_once('../../private/initialize.php'); ?>
+<?php require_once('../../private/initialize.php'); ?> 
 
-<?php include('../../private/shared/staff_header.php'); ?>
+<?php $page_title = "Staff Area"; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-    <div id="content"></div>
+    <div id="content">
 
-<?php include('../../private/shared/staff_footer.php'); ?>
+        <div id="main-menu">
+            <h2>Main Menu</h2>
+            <ul>
+                <!-- forward slash means absolute url -->
+                <li><a href="genres/index.php">Genres</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
