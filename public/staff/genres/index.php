d8b29eff925b17ef510a7dc97a56a21c6c4267a0
Juli Cheng <94julicheng@gmail.com>
2018-03-12 17:40:20 +0000

    add foreach loop to output table data

--- public/staff/genres/index.php
+++ public/staff/genres/index.php
@@ -14,6 +14,38 @@ $genres = [
 <?php $page_title = "Genres"; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-    <div id="content"></div>
+    <div id="content">
+        <div class="listing subjects">
+            <h1>Genres</h1>
+
+            <div class="actions">
+                <a class="action" href="">Create New Genre</a>
+            </div>
+
+            <table class="list">
+                <tr>
+                    <th>ID</th>
+                    <th>Position</th>
+                    <th>Visible</th>
+                    <th>Genre</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
+                </tr>
+
+                <?php foreach($genres as $genre) { ?>
+                    <tr>
+                        <td><?php echo $genre['id']; ?></td>
+                        <td><?php echo $genre['position']; ?></td>
+                        <td><?php echo $genre['visible']; ?></td>
+                        <td><?php echo $genre['menu_name']; ?></td>
+                        <td><a class="action" href="<?php echo url_for('/staff/genres/show.php?id=' . $genre['id']); ?>">View</a></td>
+                        <td><a class="action" href="">Edit</a></td>
+                        <td><a class="action" href="">Delete</a></td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
