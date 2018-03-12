fa1f7a5386c73c2ee952a3656c3445ef34a12b2a
Juli Cheng <94julicheng@gmail.com>
2018-03-12 18:40:18 +0000

    add comment of difference between file path and url

--- private/initialize.php
+++ private/initialize.php
@@ -3,12 +3,14 @@
 // Assign file paths to PHP constants
 // __FILE__ returns the current path to this file
 // dirname() returns the path to the parent directory
+// ***for pulling information from a page
 define("PRIVATE_PATH", dirname(__FILE__)); 
 define("PROJECT_PATH", dirname(PRIVATE_PATH)); 
 define("PUBLIC_PATH", PROJECT_PATH . '/public');
 define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
 // Assign the root URL to a PHP constant
+// ***for links when going to a different page!
 // * Do not need to include the domain
 // * Use same document root as webserver
 // * Can set a hardcoded value:
