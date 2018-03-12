74d5856d49bea307ed09cd9f397165f3f776c0f7
Juli Cheng <94julicheng@gmail.com>
2018-03-12 18:43:02 +0000

    add styling to show pages

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,6 +1,6 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php
+<?php 
 
 $id = isset($_GET['id']) ? $_GET['id'] : '1';
 
@@ -8,15 +8,15 @@ $id = isset($_GET['id']) ? $_GET['id'] : '1';
 
 ?>
 
-<?php $page_title = "Show Genre" ?>
+<?php $page_title = "Show Page" ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
-    <a class="back-link" href="<?php echo url_for('staff/genres/index.php'); ?>">&laquo; Back to List</a>
+    <a class="back-link" href="<?php echo url_for('staff/pages/index.php'); ?>">&laquo; Back to List</a>
 
-    <div class="subject show">
-        Genre ID: <?php echo h($id); ?>
+    <div class="page show">
+        Page ID: <?php echo h($id); ?>
     </div>
 
 </div>
