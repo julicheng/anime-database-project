6047c8e898b91c6967bfcd23e0099815e32cd20d
Juli Cheng <94julicheng@gmail.com>
2018-03-13 14:33:43 +0000

    add find, insert and update functions for genres

--- private/query_functions.php
+++ private/query_functions.php
@@ -11,6 +11,61 @@
         return $result;
     }
 
+    function find_genre_by_id($id) {
+        global $db; //db not in scope so need to global it
+
+        $sql = "SELECT * FROM genres ";
+        $sql.= "WHERE id='" . $id . "'"; //finds id
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+
+        $genre = mysqli_fetch_assoc($result);
+        mysqli_free_result($result); //free result because we have assigned result to a variable
+        return $genre; //returns an assoc array
+    }
+
+    function insert_genre($menu_name, $position, $visible) {
+        global $db;
+
+        $sql = "INSERT INTO genres ";
+        $sql.= "(menu_name, position, visible) ";
+        $sql.= "VALUES (";
+        $sql.= "'" . $menu_name . "', ";
+        $sql.= "'" . $position . "', ";
+        $sql.= "'" . $visible . "'";
+        $sql.= ")";
+        $result = mysqli_query($db, $sql);
+
+        // for insert statement ^ $result is true/false
+        if($result) {
+           return true;
+        } else {
+            echo mysqli_error($db);
+            db_disconnect($db);
+        }
+    }
+
+    function update_genre($genre) {
+        global $db;
+
+        $sql = "UPDATE genres SET ";
+        $sql.= "menu_name='" . $genre['menu_name'] . "', "; //uses variables above
+        $sql.= "position='" . $genre['position'] . "', ";
+        $sql.= "visible='" . $genre['visible'] . "' ";
+        $sql.= "WHERE id='" . $genre['id'] . "' "; //uses the $_GET['id']
+        $sql.= "LIMIT 1";
+
+        $result = mysqli_query($db, $sql);
+        //for UPDATE statements,$result is true/false
+        if ($result) {
+            return true;
+        } else {
+            //update failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+        }
+    }
+
     function find_all_pages() {
         global $db;
 
@@ -20,4 +75,6 @@
         confirm_result_set($result);
         return $result;
     }
+
+    
 ?>
