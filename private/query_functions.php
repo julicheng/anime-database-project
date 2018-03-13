0ed72a6968b3ace78e9c4f134d0fe0b06163c929
Juli Cheng <94julicheng@gmail.com>
2018-03-13 13:32:54 +0000

    create find_all_pages function

--- private/query_functions.php
+++ private/query_functions.php
@@ -5,8 +5,19 @@
 
         $sql = "SELECT * FROM genres ";
         $sql.= "ORDER BY position ASC";
+        // echo $sql; check the string
         $result = mysqli_query($db, $sql);
+        confirm_result_set($result); // runs to check if there is a result set
         return $result;
     }
 
+    function find_all_pages() {
+        global $db;
+
+        $sql = "SELECT * FROM pages ";
+        $sql.= "ORDER BY genre_id ASC, position ASC";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        return $result;
+    }
 ?>
