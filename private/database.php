8ac4f7d70742a703c9b3175ca56b33b8f1b65798
Juli Cheng <94julicheng@gmail.com>
2018-03-13 13:33:11 +0000

    add error checking functions

--- private/database.php
+++ private/database.php
@@ -7,12 +7,30 @@ require_once('db_credentials.php');
 // connect to database
 function db_connect() {
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    confirm_db_connect(); //check if there are any connection errors
     return $connection;
 }
 
 function db_disconnect($connection) {
     if(isset($conneciton)) {
         mysqli_close($connection);
+    }
+}
+
+//check if connection was successful
+function confirm_db_connect() {
+    if(mysqli_connect_errno()) {
+        $msg = "Database connection failed: ";
+        $msg .= mysqli_connect_error();
+        $msg .= " (" . mysqli_connect_errno() . ")";
+        exit($msg);
+    }
+}
+
+//if doesn't run if there is a result set
+function confirm_result_set($result_set) {
+    if(!$result_set) {
+        exit("Database query failed.");
     }
 }
 
