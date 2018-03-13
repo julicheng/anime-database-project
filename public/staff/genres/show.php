7046eb43f575ae5e172a4ba68bd424d066ee5f09
Juli Cheng <94julicheng@gmail.com>
2018-03-13 14:31:10 +0000

    show all info about a field

--- public/staff/genres/show.php
+++ public/staff/genres/show.php
@@ -6,6 +6,8 @@ $id = isset($_GET['id']) ? $_GET['id'] : '1';
 
 // $id = $_GET['id'] ?? '1'; // PHP 7.0 or more
 
+$genre = find_genre_by_id($id);
+
 ?>
 
 <?php $page_title = "Show Genre" ?>
@@ -16,7 +18,25 @@ $id = isset($_GET['id']) ? $_GET['id'] : '1';
     <a class="back-link" href="<?php echo url_for('staff/genres/index.php'); ?>">&laquo; Back to List</a>
 
     <div class="subject show">
-        Genre ID: <?php echo h($id); ?>
+
+        <h1>Genre: <?php echo h($genre['menu_name']); ?></h1>
+
+        <div class="attributes">
+            <dl>
+                <dt>Title</dt>
+                <dd><?php echo h($genre['menu_name']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd><?php echo h($genre['position']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd><?php echo h($genre['visible']) == "1" ? "true" : "false"; ?></dd>
+            </dl>
+
+        </div>
+
     </div>
 
 </div>
