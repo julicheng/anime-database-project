9a9be5b58aaa1df9605de4e993bd161840b40e1f
Juli Cheng <94julicheng@gmail.com>
2018-03-13 17:34:20 +0000

    add read page for pages

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -6,6 +6,8 @@ $id = isset($_GET['id']) ? $_GET['id'] : '1';
 
 // $id = $_GET['id'] ?? '1'; // PHP 7.0 or more
 
+$page = find_page_by_id($id);
+
 ?>
 
 <?php $page_title = "Show Page" ?>
@@ -15,8 +17,36 @@ $id = isset($_GET['id']) ? $_GET['id'] : '1';
 
     <a class="back-link" href="<?php echo url_for('staff/pages/index.php'); ?>">&laquo; Back to List</a>
 
-    <div class="page show">
-        Page ID: <?php echo h($id); ?>
+    <div class="subject show">
+
+        <h1>Title: <?php echo h($page['menu_name']); ?></h1>
+
+        <div class="attributes">
+            <!-- returns an assoc array -->
+            <?php $genre = find_genre_by_id($page['genre_id']); ?>
+            <dl>
+                <dt>Title</dt>
+                <dd><?php echo h($page['menu_name']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Genre ID</dt>
+                <dd><?php echo h($genre['menu_name']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd><?php echo h($page['position']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd><?php echo h($page['visible']) == "1" ? "true" : "false"; ?></dd>
+            </dl>
+            <dl>
+                <dt>Content</dt>
+                <dd><?php echo h($page['content']); ?></dd>
+            </dl>
+
+        </div>
+
     </div>
 
 </div>
