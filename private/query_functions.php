dcf5bd663c08b5c24031d6a08ea042a88f56fd1e
Juli Cheng <94julicheng@gmail.com>
2018-03-13 17:33:54 +0000

    create page crud functions

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,5 +1,7 @@
 <?php 
 
+    //GENRE FUNCTIONS
+
     function find_all_genres() {
         global $db; //need to pass $db as not in scope
 
@@ -88,6 +90,8 @@
         }
     }
 
+    // PAGE FUNCTIONS
+
     function find_all_pages() {
         global $db;
 
@@ -98,5 +102,83 @@
         return $result;
     }
 
+    function find_page_by_id($id) {
+        global $db;
+
+        $sql = "SELECT * FROM pages ";
+        $sql.= "WHERE id='" . $id . "'";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+
+        $page = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        return $page;
+    }
+
+    function insert_page($page) {
+        global $db;
+
+        $sql = "INSERT INTO pages ";
+        $sql.= "(genre_id, menu_name, position, visible, content) ";
+        $sql.= "VALUES (";
+        $sql.= "'" . $page['genre_id'] . "',";
+        $sql.= "'" . $page['menu_name'] . "',";
+        $sql.= "'" . $page['position'] . "',";
+        $sql.= "'" . $page['visible'] . "',";
+        $sql.= "'" . $page['content'] . "'";
+        $sql.= ")";
+        $result = mysqli_query($db, $sql);
+
+        if($result) {
+           return true;
+        } else {
+            //insert failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit();
+        }
+    }
+
+    function update_page($page) {
+        global $db;
+
+        $sql = "UPDATE pages SET ";
+        $sql.= "genre_id='" . $page['genre_id'] . "', ";
+        $sql.= "menu_name='" . $page['menu_name'] . "', ";
+        $sql.= "position='" . $page['position'] . "', ";
+        $sql.= "visible='" . $page['visible'] . "', ";
+        $sql.= "content='" . $page['content'] . "' ";
+        $sql.= "WHERE id='" . $page['id'] . "' ";
+        $sql.= "LIMIT 1";
+
+        $result = mysqli_query($db, $sql);
+
+        if($result) {
+           return true;
+        } else {
+            //insert failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit();
+        }
+    }
+
+    function delete_page($id) {
+        global $db;
+
+        $sql = "DELETE FROM pages ";
+        $sql.= "WHERE id='" . $id . "' ";
+        $sql.= "LIMIT 1";
+        $result = mysqli_query($db, $sql);
+        
+        if($result) {
+           return true;
+        } else {
+            //insert failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit();
+        }
+    }
     
 ?>
