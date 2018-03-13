84f935c0e6fe88a64ac96c051417e95298fe03b3
Juli Cheng <94julicheng@gmail.com>
2018-03-13 15:16:19 +0000

    convert arguements to array for create.php

--- public/staff/genres/create.php
+++ public/staff/genres/create.php
@@ -4,11 +4,12 @@ require_once('../../../private/initialize.php');
 if(is_post_request()) {
 // handle form values sent by new.php
 
-$menu_name = isset($_POST['menu_name']) ? $_POST['menu_name'] : "";
-$position = isset($_POST['position']) ? $_POST['position'] : "";
-$visible = isset($_POST['visible']) ? $_POST['visible'] : "";
+$genre = [];
+$genre['menu_name'] = isset($_POST['menu_name']) ? $_POST['menu_name'] : "";
+$genre['position'] = isset($_POST['position']) ? $_POST['position'] : "";
+$genre['visible'] = isset($_POST['visible']) ? $_POST['visible'] : "";
 
-$result = insert_genre($menu_name, $position, $visible); //use above variables in function
+$result = insert_genre($genre); //use above variables in function
 $new_id = mysqli_insert_id($db); //check the new id that has been created
 redirect_to(url_for('/staff/genres/show.php?id=' . $new_id)); //use the new id and load show page
 
