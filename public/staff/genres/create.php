7d344d845e4f14d6dcfb6c71f392fa9d720f08aa
Juli Cheng <94julicheng@gmail.com>
2018-03-13 19:18:51 +0000

    realise multiple form submission not good for error handling

--- public/staff/genres/create.php
+++ public/staff/genres/create.php
@@ -10,10 +10,15 @@ $genre['position'] = isset($_POST['position']) ? $_POST['position'] : "";
 $genre['visible'] = isset($_POST['visible']) ? $_POST['visible'] : "";
 
 $result = insert_genre($genre); //use above variables in function
-$new_id = mysqli_insert_id($db); //check the new id that has been created
-redirect_to(url_for('/staff/genres/show.php?id=' . $new_id)); //use the new id and load show page
-
+if($result === true) {
+    $new_id = mysqli_insert_id($db); //check the new id that has been created
+    redirect_to(url_for('/staff/genres/show.php?id=' . $new_id)); //use the new id and load show page
+} else {
+    $errors = $result;
+}
 } else {
     redirect_to(url_for('/staff/genres/new.php'));
 }
+
+// ERROR HANDLING NOT GOOD FOR MULTIPLE FORM SUBMISSION
 ?>
