3f57677c59fe03383ee60472a2ff517172450746
Juli Cheng <94julicheng@gmail.com>
2018-03-16 21:15:37 +0000

    code for skipping hidden genres and pages

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -10,6 +10,7 @@ $genre_id = isset($genre_id) ? $genre_id : "";
     <?php $nav_genres = find_all_genres(); ?>
     <ul class="subjects">
         <?php while($nav_genre = mysqli_fetch_assoc($nav_genres)) { ?>
+            <?php // if(!nav_genre['visible']) { continue; } ?>
             <li class="<?php if($nav_genre['id'] == $genre_id) { echo "selected"; } ?>">
                 <a href="<?php echo url_for('index.php?genre_id=' . h(u($nav_genre['id']))); ?>">
                 <?php echo h($nav_genre['menu_name']); ?>
@@ -18,6 +19,7 @@ $genre_id = isset($genre_id) ? $genre_id : "";
                 <?php if($nav_genre['id'] == $genre_id) { ?> 
                     <?php $nav_pages = find_pages_by_genre_id($nav_genre['id']); ?>
                     <ul class="pages">
+                        <?php // if(!nav_page['visible]) { continue; } ?>
                         <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
                             <li class="<?php if ($nav_page['id'] == $page_id) { echo "selected";} ?>">
                                 <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
